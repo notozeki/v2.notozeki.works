cf69ab49927dbf4d6cc07b03cd0620596d977729
notozeki <notozeki@gmail.com>
2019-01-14 21:05:31 +0900

    Add basic SNS <meta> tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 const pkg = require('./package')
+const url = 'https://v2.notozeki.works/' // WIP
 
 module.exports = {
   srcDir: 'app',
@@ -13,7 +14,20 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+
+      // Twitter Cards tags
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@notozeki' },
+      { hid: 'twitter:title', name: 'twitter:title', content: pkg.name },
+      { hid: 'twitter:description', name: 'twitter:description', content: pkg.description },
+      { hid: 'twitter:image', name: 'twitter:image', content: url + 'card-alpha.png' },
+
+      // OGP tags
+      { hid: 'og:url', property: 'og:url', content: url },
+      { hid: 'og:title', property: 'og:title', content: pkg.name },
+      { hid: 'og:description', property: 'og:description', content: pkg.description },
+      { hid: 'og:image', property: 'og:image', content: url + 'card-alpha.png' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
