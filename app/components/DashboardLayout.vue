92d4a7bab1e8c2baba26c8d383fd21218586b350
notozeki <notozeki@gmail.com>
2019-02-04 15:47:09 +0900

    Add the sidebar of the dashboard w/ layout improvement

--- app/components/DashboardLayout.vue
+++ app/components/DashboardLayout.vue
@@ -1,23 +1,46 @@
 <template>
-  <div>
-    <navbar />
-
-    <aside>
-      aside
-    </aside>
-
-    <main>
-      <slot />
+  <div id="dashboard">
+    <navbar id="navbar"/>
+    <sidebar id="sidebar"/>
+    <main id="main">
+      <slot/>
     </main>
   </div>
 </template>
 
 <script>
 import DashboardNavbar from '~/components/DashboardNavbar'
+import DashboardSidebar from '~/components/DashboardSidebar'
 
 export default {
   components: {
     navbar: DashboardNavbar,
+    sidebar: DashboardSidebar,
   },
 }
 </script>
+
+<style lang="scss" scoped>
+#dashboard {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid:
+    " navbar  navbar" 52px
+    " sidebar main  " 1fr
+    / 256px   1fr;
+}
+
+#navbar {
+  grid-area: navbar;
+}
+
+#sidebar {
+  grid-area: sidebar;
+}
+
+#main {
+  grid-area: main;
+  overflow: auto;
+}
+</style>
---
+++ app/components/DashboardSidebar.vue
@@ -0,0 +1,43 @@
+<template>
+  <aside class="sidebar uk-light">
+    <h3 class="sidebar-section title">
+      Dashboard
+    </h3>
+    <ul class="sidebar-section uk-nav uk-nav-default">
+      <li class="uk-nav-header">Contents</li>
+      <li><a href="#todo"><span uk-icon="icon: pencil"/> Drawings</a></li>
+    </ul>
+  </aside>
+</template>
+
+<style lang="scss" scoped>
+.sidebar {
+  width: 100%;
+  height: 100%;
+  background-color: #263238;
+}
+
+.sidebar-section {
+  border-bottom: 1px solid #546E7A;
+}
+
+.title {
+  margin: 0;
+  padding: 12px 15px;
+  font-size: 1.1rem;
+}
+
+.uk-nav.sidebar-section {
+  padding: 8px 0;
+
+  > li > a {
+    padding: 10px 15px;
+  }
+
+  .uk-nav-header {
+    padding: 6px 15px;
+    margin: 0;
+  }
+}
+
+</style>
